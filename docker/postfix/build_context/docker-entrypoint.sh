7ed54ae98d20e09fa3195afab7c05a02cb94c696
vph <vph.persson@gmail.com>
2023-05-26 22:50:49 +0200

    Add configuration files.

--- docker/postfix/build_context/docker-entrypoint.sh
+++ docker/postfix/build_context/docker-entrypoint.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
+REGEX='^.+ postfix/smtpd\[[0-9]+\]: \[([^]]+)\]:[0-9]+ [<>] [^[]+\[([^]]+)\]:([0-9]+): (.+)$'
+
 if [[ -z "$POSTFIX_HOSTNAME" ]]; then
   echo 'The environment variable POSTFIX_HOSTNAME must be set.' 1>&2
   exit 1
 fi
 
-echo "$POSTFIX_HOSTNAME" > /etc/hostname
-
-REGEX='^.+ postfix/smtpd\[[0-9]+\]: \[([^]]+)\]:[0-9]+ [<>] [^[]+\[([^]]+)\]:([0-9]+): (.+)$'
+cp /etc/postfix/main_host.cf /etc/postfix/main.cf
 
 newaliases
+postconf -e "myhostname = ${POSTFIX_HOSTNAME}"
 
 postfix start-fg | tee /dev/stderr | while IFS= read line; do
     if [[ $line =~ $REGEX ]]; then
